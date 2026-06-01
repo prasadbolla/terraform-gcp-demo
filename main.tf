26a94610383e4993049c30ff234e75e634360832
prasadbolla <bollaprasad@gmail.com>
2026-06-01 16:54:06 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "demo" {
+  name         = "terraform-demo-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+}
