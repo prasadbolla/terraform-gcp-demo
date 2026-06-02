f9338f01d958d2228b30419b443c499c16f42d95
prasadbolla <bollaprasad@gmail.com>
2026-06-02 09:45:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "demo" {
   name         = "terraform-demo-vm"
-  machine_type = "e2-micro"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
 
   boot_disk {
